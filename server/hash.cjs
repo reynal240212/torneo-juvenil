a1eeb6e1f4fcf97869cd72c881804d7aaab57feb
reynal240212 <reynal2402@gmail.com>
2025-11-05 22:40:36 -0500

    se retira el backedn y se guarda en una carpeta aparte

--- server/hash.cjs
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-const password = 'admin24120627'; // Cambia por tu password real
-
-async function run() {
-  const hash = await bcrypt.hash(password, 10);
-  console.log('HASH GENERADO:', hash);
-}
-run();
